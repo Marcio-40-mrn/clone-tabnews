0ddf54a893e0774ecbd1a9c32a21348be7e2c59e
Marcio Rodrigo de Paula Nascimento <informatica.mrn@gmail.com>
2024-05-22 16:37:25 +0000

    correção 'index.js' 22-05

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
-<h1>
-  Nossa como o tempo esta passando rápido....amanhã você já completa 8 anos de
-  vida
-</h1>;
+function Home() {
+  return (
+    <h1>
+      Nossa como o tempo esta passando rápido....amanhã você já completa 8 anos
+      de vida
+    </h1>
+  );
+}
+
+export default Home;
