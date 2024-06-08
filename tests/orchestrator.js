a663c6ee1f72fc4ea69a2e1cf934c7d87d977a35
Marcio Rodrigo de Paula Nascimento <informatica.mrn@gmail.com>
2024-06-08 02:06:59 +0000

    add fix-npm-test and orchestrator.js

---
+++ tests/orchestrator.js
@@ -0,0 +1,18 @@
+import retry from "async-retry";
+
+async function waitForAllServices() {
+  await waitForAllServices();
+
+  async function waitForAllServices() {
+    return retry(fetchStatusPage);
+
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
+    }
+  }
+}
+
+export default {
+  waitForAllServices,
+};
